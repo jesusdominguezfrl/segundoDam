3c1e41b92b15f6720340aa622fef5f94605dc4e8
Jesus <Jesus@DESKTOP-FND6VUH>
2017-12-03 19:40:57 +0100

    Conectores JDBC

--- Ejercicio7ADDJDBCConeector/src/ejercicio7addjdbcconeector/Ejercicio7ADDJDBCConeector.java
+++ Ejercicio7ADDJDBCConeector/src/ejercicio7addjdbcconeector/Ejercicio7ADDJDBCConeector.java
@@ -25,29 +25,38 @@ public class Ejercicio7ADDJDBCConeector {
         Scanner leer= new Scanner(System.in);
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            try (Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/ejemplo", "root", "123456");
-                    Statement sentencia = conexion.createStatement();
-                    ResultSet resul = sentencia.executeQuery("SELECT * FROM departamentos")) {
-                while (resul.next()) {
-                    System.out.println(resul.getInt(1) + " " + resul.getString(2) + " " + resul.getNString(3));
-                }
-            }
+            try (Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/ejemplo", "root", "123456"))
+                     {
+                         do {                             
+                            System.out.println("Elige una consulta:\n\t1-Apellidos de los empleados del departamento Ventas.\n\t2-Apellidos de todos los empleados contratados entre 1980-1990\n\t3-Nombre de departamento y suma de los salarios\n\t0-Salir");
+                            System.out.println("Introduzca una opcion: ");
+                            opcion= Integer.parseInt(leer.nextLine()); 
+                            Statement sentencia = conexion.createStatement();
+                            ResultSet resul=null;
+                            switch(opcion){
+                                case 1:
+                                    System.out.println("Apellidos de los empleados del departamento Ventas.");
+                                    resul = sentencia.executeQuery("SELECT apellido from empleados inner join departamentos ON departamentos.dept_no=empleados.dept_no where departamentos.dnombre='VENTAS' ;");
+                                    break;
+                                case 2:
+                                    System.out.println("Apellidos de todos los empleados contratados entre 1980-1990");
+                                    break;
+                                case 3:
+                                    System.out.println("Nombre de departamento y suma de los salarios");
+                                    break;
+                            }
+                            if(resul!=null){
+                                System.out.println("****************************************");
+                                while (resul.next()) {
+                                    System.out.println(resul.getInt(1) + " " + resul.getString(2) + " " + resul.getNString(3));
+                                }
+                                System.out.println("****************************************");
+                            }
+                         } while (opcion!=0);
+                    }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-
-        do {            
-            System.out.println("Elige una consulta:\n\t1-Apellidos de los empleados del departamento Ventas.\n\t2-Apellidos de todos los empleados contratados entre 1980-1990\n\t3-Nombre de departamento y suma de los salarios\n\t0-Salir");
-            System.out.println("Introduzca una opcion: ");
-            opcion= Integer.parseInt(leer.nextLine());
-            
-        } while (true);
-        
-        
-        
-        
-        
-
     }
 
 }
