97bd7ed554df260e4398be3e79497ff7c9b419b5
usuario <usuario@usuario-PC>
2017-12-04 12:27:27 +0100

    Ejercicio7 conector JDBC

--- Ejercicio7ADDJDBCConeector/src/ejercicio7addjdbcconeector/Ejercicio7ADDJDBCConeector.java
+++ Ejercicio7ADDJDBCConeector/src/ejercicio7addjdbcconeector/Ejercicio7ADDJDBCConeector.java
@@ -33,25 +33,36 @@ public class Ejercicio7ADDJDBCConeector {
                             opcion= Integer.parseInt(leer.nextLine()); 
                             Statement sentencia = conexion.createStatement();
                             ResultSet resul=null;
+                            System.out.println("****************************************");
                             switch(opcion){
                                 case 1:
                                     System.out.println("Apellidos de los empleados del departamento Ventas.");
-                                    resul = sentencia.executeQuery("SELECT apellido from empleados inner join departamentos ON departamentos.dept_no=empleados.dept_no where departamentos.dnombre='VENTAS' ;");
+                                    resul = sentencia.executeQuery("SELECT apellido from empleados INNER JOIN departamentos ON departamentos.dept_no=empleados.dept_no WHERE departamentos.dnombre='VENTAS' ;");
+                                    while(resul.next()){
+                                        System.out.println(resul.getString(1));
+                                    }
                                     break;
                                 case 2:
                                     System.out.println("Apellidos de todos los empleados contratados entre 1980-1990");
+                                    //resul = sentencia.executeQuery("SELECT apellido FROM empleados WHERE YEAR(feha_alta)>1979 AND YEAR(feha_alta)<1991;");//Error campo columna
+                                    resul = sentencia.executeQuery("SELECT apellido FROM empleados WHERE YEAR(fecha_alta)>1979 AND YEAR(fecha_alta)<1991;");
+                                    while(resul.next()){
+                                        System.out.println(resul.getString(1));
+                                    }
                                     break;
                                 case 3:
                                     System.out.println("Nombre de departamento y suma de los salarios");
+                                    resul = sentencia.executeQuery("SELECT departamentos.dnombre, SUM(empleados.salario) AS SumaPorDepartamento \n" +
+                                                                    "FROM departamentos \n" +
+                                                                    "INNER JOIN empleados ON empleados.dept_no=departamentos.dept_no \n" +
+                                                                   "GROUP BY departamentos.dnombre;");
+                                    while(resul.next()){
+                                        System.out.printf("%-20s\t%6.2f\n",resul.getString(1),resul.getFloat(2));
+                                    }
                                     break;
                             }
-                            if(resul!=null){
                                 System.out.println("****************************************");
-                                while (resul.next()) {
-                                    System.out.println(resul.getInt(1) + " " + resul.getString(2) + " " + resul.getNString(3));
-                                }
-                                System.out.println("****************************************");
-                            }
+                            
                          } while (opcion!=0);
                     }
         } catch (Exception e) {
---
+++ Ejercicio7ADDJDBCConeector/values_empl.sql
@@ -0,0 +1,12 @@
+INSERT INTO empleados VALUES (1, 'López', 'contable', 345, '1987-10-23', 23400.32, 5.34, 10);
+INSERT INTO empleados VALUES (2, 'Alonso', 'contable', 346, '1994-12-23', 20400.32, 3.34, 10);
+INSERT INTO empleados VALUES (3, 'Santana', 'contable', 345, '1980-09-23', 33670.20, 8.84, 10);
+INSERT INTO empleados VALUES (4, 'Gil', 'investigad', 245, '1987-01-23', 23400.32, 5.34, 20);
+INSERT INTO empleados VALUES (5, 'Lorenzo', 'investigad', 246, '1992-12-23', 20400.32, 3.34, 20);
+INSERT INTO empleados VALUES (6, 'Manteca', 'investigad', 245, '2000-10-23', 33670.20, 8.84, 20);
+INSERT INTO empleados VALUES (7, 'Tocino', 'vendedor', 445, '2003-05-23', 23400.32, 5.34, 30);
+INSERT INTO empleados VALUES (8, 'Malo', 'vendedor', 446, '1994-06-23', 20400.32, 3.34, 30);
+INSERT INTO empleados VALUES (9, 'Salamanca', 'vendedor', 445, '2004-08-23', 33670.20, 8.84, 30);
+INSERT INTO empleados VALUES (10, 'Iglesias', 'productor', 145, '1987-01-23', 33400.32, 15.34, 40);
+INSERT INTO empleados VALUES (11, 'Martín', 'productor', 146, '2004-12-23', 28400.32, 13.34, 40);
+INSERT INTO empleados VALUES (12, 'Soroya', 'productor', 145, '1980-10-23', 43670.20, 18.84, 40);
