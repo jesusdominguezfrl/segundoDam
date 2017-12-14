81452a0c9e134bfa8d780b1916c5957957716ee1
usuario <usuario@usuario-PC>
2017-12-14 11:49:09 +0100

    Ejercicio2

---
+++ ServiciosEJ2SocketsCliente/src/serviciosej2socketscliente/ServiciosEJ2SocketsCliente.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serviciosej2socketscliente;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ *
+ * @author usuario
+ */
+public class ServiciosEJ2SocketsCliente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+      String HOST = "localhost";
+        int PUERTO = 50000;
+        try{
+            System.out.println("PROGRAMA CLIENTE INICIADO");
+            Socket Cliente = new Socket(HOST, PUERTO);
+            OutputStream salida = Cliente.getOutputStream();
+            DataOutputStream flujoSalida = new DataOutputStream(salida);
+            flujoSalida.writeUTF("Saludos al SERVIDOR DESDE EL CLIENTE ");
+            InputStream entrada = Cliente.getInputStream();
+            DataInputStream flujoEntrada = new DataInputStream(entrada);
+            System.out.println("Recibiendo del SERVIDOR:\n\t" + flujoEntrada.readUTF());
+
+            entrada.close();
+            flujoEntrada.close();
+            salida.close();
+            flujoSalida.close();
+            Cliente.close();
+            
+        }catch(Exception e){
+            System.out.println("Excepcion: "+e.getMessage());
+        }
+    }//MAIN
+    
+}
---
+++ ServiciosEj2SocketsServidor/src/serviciosej2socketsservidor/ServiciosEj2SocketsServidor.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serviciosej2socketsservidor;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author usuario
+ */
+public class ServiciosEj2SocketsServidor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int PUERTO = 50000;
+        try {
+            ServerSocket Servidor = new ServerSocket(PUERTO);
+            System.out.println("Escucho el puerto " + PUERTO);
+            System.out.println("Esperando al cliente.... ");
+            Socket Clienteconectado = Servidor.accept(); 
+            InputStream entrada = Clienteconectado.getInputStream();
+            DataInputStream flujoEntrada = new DataInputStream(entrada);
+            System.out.println("Recibiendo del CLIENTE:\n\t" + flujoEntrada.readUTF());
+            OutputStream salida = Clienteconectado.getOutputStream();
+            DataOutputStream flujoSalida = new DataOutputStream(salida);
+            flujoSalida.writeUTF("Saludos al cliente del servidor ");
+
+            entrada.close();
+            flujoEntrada.close();
+            salida.close();
+            flujoSalida.close();
+            Clienteconectado.close();
+            Servidor.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+    
+}
