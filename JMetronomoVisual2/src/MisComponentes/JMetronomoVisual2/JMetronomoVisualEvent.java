1974d528443062dfed3aca130d1dcbd64bd35e75
Jesus <Jesus@DESKTOP-FND6VUH>
2017-12-26 21:55:16 +0100

    Componente Metronomo visual OK

---
+++ JMetronomoVisual2/src/MisComponentes/JMetronomoVisual2/JMetronomoVisualEvent.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MisComponentes.JMetronomoVisual2;
+
+import java.util.EventObject;
+
+/**
+ *
+ * @author Jesus
+ */
+public class JMetronomoVisualEvent extends EventObject{
+    
+    public JMetronomoVisualEvent(Object source) {
+        super(source);
+    }
+    
+}
