3e982d9b0d247f019cbc798e552b4a99532ee633
usuario <usuario@usuario-PC>
2018-01-24 14:13:25 +0100

    Ej1ConexionAServidorHibernate

--- Ej1ConexionAServidor/src/ej1conexionaservidor/Ej1ConexionAServidor.java
+++ Ej1ConexionAServidor/src/ej1conexionaservidor/Ej1ConexionAServidor.java
@@ -5,6 +5,14 @@
  */
 package ej1conexionaservidor;
 
+import java.util.Scanner;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import primero.Departamentos;
+import primero.Empleados;
+import primero.SessionFactoryUtil;
+
 /**
  *
  * @author usuario
@@ -15,7 +23,22 @@ public class Ej1ConexionAServidor {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        SessionFactory sesion = SessionFactoryUtil.getSessionFactory();
+        Session session = sesion.openSession();
+        Transaction tx = session.beginTransaction();
+        System.out.println("Inserción de una fila en la tabla Departamentos.");
+        Scanner scr = new Scanner(System.in);
+        Departamentos depart = new Departamentos();
+        System.out.println("Número departamento: ");
+        depart.setDeptNo((byte) Integer.parseInt(scr.nextLine()));
+        System.out.println("Nombre departamento: ");
+        depart.setDnombre(scr.nextLine());
+        System.out.println("Localización: ");
+        depart.setLoc(scr.nextLine());//Inserción de un departamento
+
+        session.save(depart);
+        tx.commit();
+        session.close();
     }
-    
+
 }
