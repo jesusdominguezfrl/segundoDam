e12f3c56d3db4f984fef831b7ec139407cd16fe8
usuario <usuario@usuario-PC>
2018-01-30 11:10:35 +0100

    Conxion FTP

--- Ej1ServidorFTPConexion/src/ej1servidorftpconexion/Ej1ServidorFTPConexion.java
+++ Ej1ServidorFTPConexion/src/ej1servidorftpconexion/Ej1ServidorFTPConexion.java
@@ -5,7 +5,10 @@
  */
 package ej1servidorftpconexion;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
+import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPReply;
 
@@ -27,16 +30,31 @@ public class Ej1ServidorFTPConexion {
 //        String servFTP = "ftp.jcyl.es"; 
         String servFTP = "10.1.4.208"; 
         FTPClient cliente = new FTPClient();
+        
         try {
             cliente.connect(servFTP);
+            cliente.login("usuario","123456");
             System.out.print(cliente.getReplyString());
             System.out.println("");
+//            cliente.changeWorkingDirectory("/Subir");//Cambiar directorio de trabajo
+            System.out.println("Se cambió satisfactoriamente el directorio");
+            //Activar que se envie cualquier tipo de archivo
+            
+            cliente.setFileType(FTP.BINARY_FILE_TYPE);
+            BufferedInputStream buffIn = null;
+            buffIn = new BufferedInputStream(new FileInputStream("C://prueba/hola.txt"));//Ruta del archivo para enviar
+            cliente.enterLocalActiveMode();
+            cliente.storeFile("hola.txt", buffIn);//Ruta completa de alojamiento en el FTP
+//            cliente.completePendingCommand();
+            buffIn.close(); //Cerrar envio de arcivos al FTP
+            cliente.logout(); //Cerrar sesión
             respuesta = cliente.getReplyCode();
             if (!FTPReply.isPositiveCompletion(respuesta)) {
                 cliente.disconnect();
             }
             
         } catch (Exception e) {
+            System.out.println(e.getMessage());
                 cliente.disconnect();
         }
 
