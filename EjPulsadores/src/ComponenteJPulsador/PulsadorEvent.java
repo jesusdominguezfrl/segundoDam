9d7f8b2541c544e5b4f2f1bbdebe332c3ae5844e
Jesus <Jesus@DESKTOP-FND6VUH>
2018-02-01 21:39:01 +0100

    Ej Pulsador Examen2017

---
+++ EjPulsadores/src/ComponenteJPulsador/PulsadorEvent.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ComponenteJPulsador;
+
+import java.util.EventObject;
+
+/**
+ *
+ * @author Jesus
+ */
+public class PulsadorEvent extends EventObject {
+    
+    public PulsadorEvent(Object source) {
+        super(source);
+    }
+    
+}
